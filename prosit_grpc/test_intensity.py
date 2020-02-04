c809514c2f2b4b22a749ee6682755956c6432dcd
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2020-02-04 10:56:11 +0100

    replaced random aa sequence with test set

--- prosit_grpc/test_intensity.py
+++ prosit_grpc/test_intensity.py
@@ -1,10 +1,24 @@
-
+import csv
 from prosit_grpc.predictPROSIT_2 import PredictPROSIT
 
+with open("../tests/input_test.csv", "r") as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+
+    header = next(reader, None)
+
+    sequences = []
+    ce = []
+    charge = []
+
+    for line in reader:
+        sequences.append(line[0])
+        ce.append(int(line[1]))
+        charge.append(int(line[2]))
+
 predictor = PredictPROSIT(server="131.159.152.7:8500",
-                          sequences_list=["AAAAAKAK","AAAAAA"],
-                          charges_list=[1,2],
-                          collision_energies_list=[25,25],
+                          sequences_list=sequences,
+                          charges_list=charge,
+                          collision_energies_list=ce,
                           model_name="intensity_prosit_publication"
                           )
 # Get predictions
@@ -12,4 +26,4 @@ predictor = PredictPROSIT(server="131.159.152.7:8500",
 print("=============================================")
 output = predictor.get_predictions()
 print("=============================================")
-print(output)
+print(list(output))
