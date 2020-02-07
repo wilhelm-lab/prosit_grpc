implemented calculation of fragment masses and added a test to check if they are correct

prosit_grpc/predictPROSIT.py
@@ -14,7 +14,7 @@ from tensorflow_serving.apis import predict_pb2, prediction_service_pb2_grpc
 from typing import Iterable, Optional, Union
 
 from . import __constants__ as C  # For constants
-from .__utils__ import normalize_intensities, indices_to_one_hot,map_peptide_to_numbers  # For ion mass computation
+from .__utils__ import compute_ion_masses,normalize_intensities, indices_to_one_hot,map_peptide_to_numbers  # For ion mass computation
 
 class PredictPROSIT:
     def __init__(self,
@@ -51,10 +51,13 @@ class PredictPROSIT:
         # prediction input instructions
         self.sequences_list = sequences_list
         self.charges_list = charges_list
-
         if self.model_type == "intensity":
-               self.collision_energies_list = [i / 100 for i in collision_energies_list]
+            self.collision_energies_list = [i / 100 for i in collision_energies_list]
+
         self.num_seq = len(sequences_list)
+
+
+
 
         # prepared/encoded input instructions
         # set with seperate function
@@ -164,6 +167,12 @@ class PredictPROSIT:
     def set_sequences_array_float32(self):
         self.sequences_array = np.array(self.sequences_list_numeric).astype(np.float32)
 
+    def set_fragment_masses(self):
+        self.fragment_masses = []
+        for i in range(self.num_seq):
+            self.fragment_masses.append(compute_ion_masses(seq_int= self.sequences_list_numeric[i],
+                                                           charge_onehot=self.charges_list_one_hot[i]))
+
     @staticmethod
     def reshape_predict_response_to_raw_predictions(predict_response, model_type):
         if model_type == "intensity":
@@ -236,12 +245,14 @@ class PredictPROSIT:
 
     def predict(self):
         self.set_sequence_list_numeric()
-
         batch_start = 0
 
         if self.model_type == "intensity":
             # set charges to one hot
             self.set_charges_list_one_hot()
+            # set fragment masses
+            self.set_fragment_masses()
+
 
             # set numpy arrays
             self.set_charges_array_float32()
@@ -323,3 +334,8 @@ class PredictPROSIT:
         if self.predictions_done == False:
             self.predict()
         return self.predictions
+
+    def get_fragment_masses(self):
+        if self.predictions_done == False:
+            self.predict()
+        return self.fragment_masses

tests/test.py
@@ -8,6 +8,8 @@ with h5py.File("data.hdf5", 'r') as f:
     intensities = list(f["intensities_pred"])
     irt = list(f["iRT"])
     irt = [i[0] for i in irt]
+    masses = list(f["masses_pred"])
+
 
 with open("input_test.csv", "r") as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
@@ -39,12 +41,7 @@ def test_array_size():
     for i in range(len(pred)):
         assert len(pred[i]) == 174
 
-def test_sequence_alpha_to_numbers():
-    result=predictor.sequence_alpha_to_numbers("ACDEFGHIKLMNPQRSTVWYUO")
-    target= [x+1 for x in range(22)]
-    assert result == target
-
-def test_intensity():
+def test_intensity_prediction():
     predictor = PredictPROSIT(server="131.159.152.7:8500",
                               sequences_list=sequences,
                               charges_list=charge,
@@ -52,14 +49,21 @@ def test_intensity():
                               model_name="intensity_prosit_publication"
                               )
     pred = predictor.get_predictions()
+    mymasses = predictor.get_fragment_masses()
+
+    print(mymasses)
 
     assert len(intensities) == len(pred)
     for i in range(len(pred)):
         pearson_correlation = np.corrcoef(intensities[i], pred[i])[0,1]
         assert round(pearson_correlation, 12) == 1
 
+    for i in range(len(pred)):
+        pearson_correlation = np.corrcoef(masses[i], mymasses[i])[0,1]
+        assert round(pearson_correlation, 12) == 1
 
-def test_proteotypicity():
+
+def test_proteotypicity_prediction():
     predictor = PredictPROSIT(server="131.159.152.7:8500",
                               sequences_list= ["THDLGKW", "VLQKQFFYCTMEKWNGRT", "QMQCNWNVMQGAPSMTCEHRVEYSMEWIID"],
                               model_name="proteotypicity"
@@ -73,7 +77,7 @@ def test_proteotypicity():
     for i in range(3):
         assert round(pred[i], 3) == target[i]
 
-def test_batching():
+def test_batched_prediction():
     predictor = PredictPROSIT(server="131.159.152.7:8500",
                               sequences_list=["THDLGKW" for i in range(10000)],
                               charges_list= [2 for i in range(10000)],
@@ -97,7 +101,7 @@ def test_batching():
     pred = predictor.get_predictions()
     assert len(pred) == 10000
 
-def test_irt():
+def test_irt_prediction():
     predictor = PredictPROSIT(server="131.159.152.7:8500",
                               sequences_list=sequences,
                               model_name="iRT"