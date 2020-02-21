bde7fbe12111ce7668bc3f23ae85343f84cf469a
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2020-02-21 15:14:47 +0100

    finished input-sub-classes and corresponding tests

--- prosit_grpc/predictPROSIT.py
+++ prosit_grpc/predictPROSIT.py
@@ -47,74 +47,152 @@ class PROSITpredictor:
         self.input = PROSITinput()
 
 class PROSITinput:
-    def __init__(self, sequences = None, charges = None, collision_energies = None):
+    def __init__(self, sequences=None, charges=None, collision_energies=None):
         self.sequences = PROSITsequences(sequences)
         self.charges = PROSITcharges(charges)
         self.collision_energies = PROSITcollisionenergies(collision_energies)
 
 class PROSITcharges:
     def __init__(self, charges):
-        self.numeric
-        self.onehot
-        self.array
+        self.numeric = None
+        self.onehot = None
+        self.array = None
+
+        charge_type = PROSITcharges.determine_type(charges)
+        if charge_type == "numeric":
+            self.numeric = charges
+        elif charge_type == "one-hot":
+            self.onehot = charges
+        elif charge_type == "array":
+            self.array = charges
 
     @staticmethod
-    def determine_type():
-        pass
+    def determine_type(charges):
+        if type(charges) == np.ndarray:
+            return "array"
+        elif type(charges[1]) is list:
+            return "one-hot"
+        elif type(charges[1]) is int:
+            return "numeric"
 
     def numeric_to_onehot(self):
-        pass
+        self.onehot = [U.indices_to_one_hot(x, C.MAX_CHARGE) for x in self.numeric]
 
     def onehot_to_array(self):
-        pass
+        self.array = np.array(self.onehot, dtype=np.int32)
+
+    def prepare_charges(self):
+        if self.array == None:
+            if self.onehot == None:
+                if self.numeric == None:
+                    raise ValueError("No charges known")
+                self.numeric_to_onehot()
+            self.onehot_to_array()
 
 class PROSITsequences:
     def __init__(self, sequences):
-        self.character
-        self.numeric
-        self.array
-        self.lengths
+
+        self.character = None
+        self.numeric = None
+        self.array = None
+        self.lengths = None
+
+        seq_type = PROSITsequences.determine_type(sequences)
+        if seq_type == "character":
+            self.character = sequences
+        elif seq_type == "numeric":
+            self.numeric = sequences
+        elif seq_type == "array":
+            self.array = sequences
 
     @staticmethod
-    def determine_type():
-        pass
-
-    def numeric_to_array(self):
-        pass
+    def determine_type(sequences):
+        if type(sequences) == np.ndarray:
+            return "array"
+        elif type(sequences[1]) is str:
+            return "character"
+        else:
+            return "numeric"
 
     def character_to_numeric(self):
-        pass
+        self.numeric = []
+        for i,sequence in enumerate(self.character):
+            num_seq = U.map_peptide_to_numbers(sequence)
+            if len(num_seq) > C.SEQ_LEN:
+                raise Exception(f"The Sequence {sequence}, has {i} Amino Acids."
+                                f"The maximum number of amino acids allowed is {C.SEQ_LEN}")
+
+            while len(num_seq) < C.SEQ_LEN:
+                num_seq.append(0)
+            self.numeric.append(num_seq)
+
+    def numeric_to_array(self):
+        self.array = np.array(self.numeric, dtype=np.int32)
 
     def calculate_lengths(self):
-        pass
+        """Calculates the length of all sequences saved in an instance of PROSITsequences
+
+        :requires PROSITsequences.array
+
+        :sets PROSITsequences.lengths
+        """
+        self.lengths = []
+        for sequence in self.array:
+            counter = 0
+            for aa in sequence:
+                if aa != 0:
+                    counter += 1
+            self.lengths.append(counter)
+
+    def prepare_sequences(self):
+        if self.array == None:
+            if self.numeric == None:
+                if self.character == None:
+                    raise ValueError("No Sequences known")
+                self.character_to_numeric()
+            self.numeric_to_array()
+            self.calculate_lengths()
 
 class PROSITcollisionenergies:
     def __init__(self, collision_energies):
+        self.numeric = None
+        self.procentual = None
+        self.array = None
 
-        type = PROSITcollisionenergies.determine_type(collision_energies)
-
-        self.numeric
-        self.procentual
-        self.array
+        ce_type = PROSITcollisionenergies.determine_type(collision_energies)
+        if ce_type == "numeric":
+            self.numeric = collision_energies
+        elif ce_type == "procentual":
+            self.procentual = collision_energies
+        elif ce_type == "array":
+            self.array = collision_energies
+        else:
+            raise ValueError("The ce_type is not known")
 
     @staticmethod
     def determine_type(collision_energies):
         if type(collision_energies) == np.ndarray:
             return "array"
-
         elif collision_energies[1] < 1:
             return "procentual"
-
         else:
             return "numeric"
 
-
     def numeric_to_procentual(self):
-        pass
+        self.procentual = [i/100 for i in self.numeric]
 
     def procentual_to_array(self):
-        pass
+        self.array = np.array(self.procentual, dtype=np.float32)
 
+    def prepare_collisionenergies(self):
+        if self.array == None:
+            if self.procentual == None:
+                if self.numeric == None:
+                    raise ValueError("No Collision Energies known")
+                self.numeric_to_procentual()
+            self.procentual_to_array()
+
+# Output
 class PROSIToutput:
     pass
 
--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,42 @@
 import numpy as np
 import prosit_grpc.predictPROSIT as prpc
 
-def test_PROSITcollisionenergies_determine_type():
+# set test variables
+charges_numeric = [1,2,3,4,5,6]
+charges_onehot = [[1, 0, 0, 0, 0, 0],
+                  [0, 1, 0, 0, 0, 0],
+                  [0, 0, 1, 0, 0, 0],
+                  [0, 0, 0, 1, 0, 0],
+                  [0, 0, 0, 0, 1, 0],
+                  [0, 0, 0, 0, 0, 1]]
+charges_array = np.array(charges_onehot, dtype=np.int32)
 
+sequences_character = ["KKVCQGTSNKLTQLGTFE",
+                       "GNMYYENSYALAVLSNYDANKTGLK",
+                       "LTQLGTFEDHFLSLQR",
+                       "FRDEATCKDTCPPLMLYNPTTQMDVNPEGK",
+                       "RDEATCKDTCPPLM(ox)LNPTTYQMDVNPEGK",
+                       "FRDEATKDTCPPLMM(O)YNPTTYQMDVNPEGK",
+                       "ANKEILDEAYVMASVDNPHVCR",
+                       "FRDEATCKDT(Cam)PPLLYNPTTYQMDVNPEGK",
+                       "FRDEATCKDTCacPPLMYNPTTYQMDVNPEGK"]
+sequences_numeric = [[9, 9, 18, 2, 14, 6, 17, 16, 12, 9, 10, 17, 14, 10, 6, 17, 5, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [6, 12, 11, 20, 20, 4, 12, 16, 20, 1, 10, 1, 18, 10, 16, 12, 20, 3, 1, 12, 9, 17, 6, 10, 9, 0, 0, 0, 0, 0],
+                     [10, 17, 14, 10, 6, 17, 5, 4, 3, 7, 5, 10, 16, 10, 14, 15, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [5, 15, 3, 4, 1, 17, 2, 9, 3, 17, 2, 13, 13, 10, 11, 10, 20, 12, 13, 17, 17, 14, 11, 3, 18, 12, 13, 4, 6, 9],
+                     [15, 3, 4, 1, 17, 2, 9, 3, 17, 2, 13, 13, 10, 21, 10, 12, 13, 17, 17, 20, 14, 11, 3, 18, 12, 13, 4, 6, 9, 0],
+                     [5, 15, 3, 4, 1, 17, 9, 3, 17, 2, 13, 13, 10, 11, 21, 20, 12, 13, 17, 17, 20, 14, 11, 3, 18, 12, 13, 4, 6, 9],
+                     [1, 12, 9, 4, 8, 10, 3, 4, 1, 20, 18, 11, 1, 16, 18, 3, 12, 13, 7, 18, 2, 15, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [5, 15, 3, 4, 1, 17, 2, 9, 3, 17, 2, 13, 13, 10, 10, 20, 12, 13, 17, 17, 20, 14, 11, 3, 18, 12, 13, 4, 6, 9],
+                     [5, 15, 3, 4, 1, 17, 2, 9, 3, 17, 2, 13, 13, 10, 11, 20, 12, 13, 17, 17, 20, 14, 11, 3, 18, 12, 13, 4, 6, 9]]
+
+sequences_array = np.array(sequences_numeric, dtype=np.int32)
+
+ce_numeric = [10,20,30,40,50]
+ce_procentual = [0.10,0.20,0.30,0.40,0.50]
+ce_array = np.array(ce_procentual, dtype=np.float32)
+
+def test_PROSITcollisionenergies_determine_type():
     # numeric
     ce = [10, 20, 30, 40, 50]
     ret = prpc.PROSITcollisionenergies.determine_type(ce)
@@ -18,4 +52,93 @@ def test_PROSITcollisionenergies_determine_type():
     ret = prpc.PROSITcollisionenergies.determine_type(ce)
     assert ret == "array"
 
-def
+def test_PROSITsequences_determine_type():
+    # character
+    seq = ["AAAAAAAA", "AAAAAAAA", "AAAAAAAA", "AAAAAAAA"]
+    ret = prpc.PROSITsequences.determine_type(seq)
+    assert ret == "character"
+
+    # numeric
+    seq = [[1,1,1,1,1,1,1], [1,1,1,1,1,1,1], [1,1,1,1,1,1,1], [1,1,1,1,1,1,1]]
+    ret = prpc.PROSITsequences.determine_type(seq)
+    assert ret == "numeric"
+
+    # array
+    seq = np.array([[1,1,1,1,1,1,1], [1,1,1,1,1,1,1], [1,1,1,1,1,1,1], [1,1,1,1,1,1,1]], dtype=np.int32)
+    ret = prpc.PROSITsequences.determine_type(seq)
+    assert ret == "array"
+
+def test_PROSITcharges_determine_type():
+    # numeric
+    ret = prpc.PROSITcharges.determine_type(charges_numeric)
+    assert ret == "numeric"
+
+    # one hot
+    ret = prpc.PROSITcharges.determine_type(charges_onehot)
+    assert ret == "one-hot"
+
+    # array
+    ret = prpc.PROSITcharges.determine_type(charges_array)
+    assert ret == "array"
+
+def test_PROSITcharges(): # test instanciation
+    #numeric
+    inst = prpc.PROSITcharges(charges_numeric)
+    assert inst.numeric == charges_numeric
+
+    # one hot
+    inst = prpc.PROSITcharges(charges_onehot)
+    assert inst.onehot == charges_onehot
+
+    # array
+    inst = prpc.PROSITcharges(charges_array)
+    assert np.array_equal(inst.array, charges_array)
+
+def test_PROSITcharges_prepare_charges():
+    inst = prpc.PROSITcharges(charges_numeric)
+    inst.prepare_charges()
+    assert inst.numeric == charges_numeric
+    assert inst.onehot == charges_onehot
+    assert np.array_equal(inst.array, charges_array)
+
+def test_PROSITsequences(): # test instanciation
+    # character
+    inst = prpc.PROSITsequences(sequences_character)
+    assert inst.character == sequences_character
+
+    # numeric
+    inst = prpc.PROSITsequences(sequences_numeric)
+    assert inst.numeric == sequences_numeric
+
+    # array
+    inst = prpc.PROSITsequences(sequences_array)
+    assert np.array_equal(inst.array, sequences_array)
+
+def test_PROSITsequences_prepare_sequences():
+    inst = prpc.PROSITsequences(sequences_character)
+    inst.prepare_sequences()
+
+    assert inst.character == sequences_character
+    assert inst.numeric == sequences_numeric
+    assert np.array_equal(inst.array, sequences_array)
+
+def test_PROSITcollisionenergies():
+    # numeric
+    inst = prpc.PROSITcollisionenergies(ce_numeric)
+    assert inst.numeric == ce_numeric
+
+    # procentual
+    inst = prpc.PROSITcollisionenergies(ce_procentual)
+    assert inst.procentual == ce_procentual
+
+    # array
+    inst = prpc.PROSITcollisionenergies(ce_array)
+    assert np.array_equal(inst.array, ce_array)
+
+def test_PROSITcollisionenergies_prepare_collisionenergies():
+    inst = prpc.PROSITcollisionenergies(ce_numeric)
+    inst.prepare_collisionenergies()
+
+    assert inst.numeric == ce_numeric
+    assert inst.procentual == ce_procentual
+    assert np.array_equal(inst.array, ce_array)
