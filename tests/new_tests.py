db327c3d7c9d22ecc2ffa69f6d959198dad675dd
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2020-02-21 10:59:01 +0100

    mid work commit because tobi interruped me

--- tests/new_tests.py
+++ tests/new_tests.py
@@ -1,18 +1,19 @@
+import numpy as np
 import prosit_grpc.predictPROSIT as prpc
 
 def test_PROSITcollisionenergies_determine_type:
 
     # numeric
-
-    prpc.PROSITcollisionenergies.determine_type()
+    ce = [10, 20, 30, 40, 50]
+    prpc.PROSITcollisionenergies.determine_type(ce)
 
 
     # procentual
-
-    prpc.PROSITcollisionenergies.determine_type()
+    ce = [0.10, 0.20, 0.30, 0.40, 0.50]
+    prpc.PROSITcollisionenergies.determine_type(ce)
 
 
     # array
-
-    prpc.PROSITcollisionenergies.determine_type()
+    ce = [0.10, 0.20, 0.30, 0.40, 0.50]
+    prpc.PROSITcollisionenergies.determine_type(ce)
 
