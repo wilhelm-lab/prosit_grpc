d96bd03921accd1b136c7019dee3d0c21fab2f71
Ludwig Lautenbacher <Ludwig.Lautenbacher@tum.de>
2020-05-22 15:00:29 +0200

    improved character to array conversion and changed dtype of sequence array to uint8 restricting the possible aa to 255

--- tests/test_PROSITinput.py
+++ tests/test_PROSITinput.py
@@ -43,7 +43,7 @@ def test_PROSITinput_prepare_input():
 
 
 def test_expand_matrices():
-    inst = prpc.PROSITinput(sequences=[[1, 1, 1, 1], [1, 1, 2, 1], [1, 2, 2, 1], [2, 2, 2, 1], [2, 2, 2, 2]],
+    inst = prpc.PROSITinput(sequences=np.array([[1, 1, 1, 1], [1, 1, 2, 1], [1, 2, 2, 1], [2, 2, 2, 1], [2, 2, 2, 2]]),
                             charges=[1, 2, 3, 4, 5],
                             collision_energies=[10, 20, 30, 40, 50])
 
