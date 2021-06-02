355b45c5a49833d25f285aa265ad7dbf1b2d1928
WassimG <wassim.gabriel@gmail.com>
2021-06-02 15:05:50 +0200

    read float precursor charge and handle nan values

--- prosit_grpc/__utils__.py
+++ prosit_grpc/__utils__.py
@@ -105,8 +105,14 @@ def indices_to_one_hot(data, nb_classes):
     Convert an iterable of indices to one-hot encoded labels.
     :param data: charge, int between 1 and 6
     """
-    targets = np.array([data-1])  # -1 for 0 indexing
-    return np.int_((np.eye(nb_classes)[targets])).tolist()[0]
+    #print(data)
+    targets = np.array([data])
+    targets = targets.astype(np.uint8)
+    targets = targets-1  # -1 for 0 indexing
+    try:
+        return np.int_((np.eye(nb_classes)[targets])).tolist()[0]
+    except IndexError:
+        raise IndexError('Please validate the precursor charge values are between 1 and 6')
 
 def generate_newMatrix_v2(
     npMatrix, iFromReplaceValue, iToReplaceValue, numberAtTheSameTime=2
